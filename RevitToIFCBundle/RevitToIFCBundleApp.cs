11780a3b5049ca7c277912d0d50c5e6a6b902d64
Simon Moreau <simon@bim42.com>
2020-09-13 22:47:50 +0200

    set up solution and local debug

---
+++ RevitToIFCBundle/RevitToIFCBundleApp.cs
@@ -0,0 +1,44 @@
+﻿using Autodesk.Revit.DB;
+using DesignAutomationFramework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitToIFCBundle
+{
+    [Autodesk.Revit.Attributes.Regeneration(Autodesk.Revit.Attributes.RegenerationOption.Manual)]
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    public class RevitToIFCBundleApp : IExternalDBApplication
+    {
+        public ExternalDBApplicationResult OnStartup(Autodesk.Revit.ApplicationServices.ControlledApplication app)
+        {
+            DesignAutomationBridge.DesignAutomationReadyEvent += HandleDesignAutomationReadyEvent;
+            return ExternalDBApplicationResult.Succeeded;
+        }
+
+        public ExternalDBApplicationResult OnShutdown(Autodesk.Revit.ApplicationServices.ControlledApplication app)
+        {
+            return ExternalDBApplicationResult.Succeeded;
+        }
+
+        public void HandleDesignAutomationReadyEvent(object sender, DesignAutomationReadyEventArgs e)
+        {
+            e.Succeeded = true;
+            DeleteAllWalls(e.DesignAutomationData);
+        }
+
+        public static void DeleteAllWalls(DesignAutomationData data)
+        {
+            File.WriteAllLines("output.txt", new string[] { "Hello"});
+            LogTrace("Saving IFC file...");
+        }
+
+        /// <summary>
+        /// This will appear on the Design Automation output
+        /// </summary>
+        private static void LogTrace(string format, params object[] args) { System.Console.WriteLine(format, args); }
+    }
+}
