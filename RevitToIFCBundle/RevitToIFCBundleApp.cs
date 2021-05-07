7b4642bb383586166c48640234f0a975be7c896d
Simon Moreau <simon@bim42.com>
2021-05-07 13:51:02 +0200

    load families in a empty proejct before exporting it

--- RevitToIFCBundle/RevitToIFCBundleApp.cs
+++ RevitToIFCBundle/RevitToIFCBundleApp.cs
@@ -19,83 +19,14 @@ namespace RevitToIFCBundle
         public ExternalDBApplicationResult OnStartup(Autodesk.Revit.ApplicationServices.ControlledApplication app)
         {
             // Hook up the CustomFailureHandling failure processor.
-            app.FailuresProcessing += FailureProcessor;
-
-            // Application.RegisterFailuresProcessor(new RevitToIFCFailuresProcessor());
+            RevitToIFCFailuresProcessor revitToIFCFailuresProcessor = new RevitToIFCFailuresProcessor();
+            app.FailuresProcessing += revitToIFCFailuresProcessor.FailureProcessor;
             RevitToIFCBundleApp.LogTrace("Custom Failures Processor registered.");
 
             DesignAutomationBridge.DesignAutomationReadyEvent += HandleDesignAutomationReadyEvent;
             return ExternalDBApplicationResult.Succeeded;
         }
 
-        private void FailureProcessor(object sender, FailuresProcessingEventArgs e)
-        {
-            RevitToIFCBundleApp.LogTrace("Enter Failures Processor event.");
-            bool hasFailure = false;
-            FailuresAccessor failureAccessor = e.GetFailuresAccessor();
-
-            List<FailureMessageAccessor> fma = failureAccessor.GetFailureMessages().ToList();
-            List<ElementId> elementsToDelete = new List<ElementId>();
-            foreach (FailureMessageAccessor failureMessageAccessor in fma)
-            {
-                FailureSeverity failureSeverity = failureAccessor.GetSeverity();
-                string message = failureMessageAccessor.GetDefaultResolutionCaption();
-
-
-                if (failureSeverity == FailureSeverity.Error)
-                {
-                    // If the default resolution for the error results in deleting the elements delete them
-                    if (failureMessageAccessor.GetDefaultResolutionCaption().Equals("Delete Element(s)"))
-                    {
-                        //use the following lines to delete the warning elements
-                        List<ElementId> FailingElementIds = failureMessageAccessor.GetFailingElementIds().ToList();
-
-                        if (FailingElementIds.Count > 0)
-                        {
-                            ElementId FailingElementId = FailingElementIds[0];
-                            if (!elementsToDelete.Contains(FailingElementId))
-                            {
-                                elementsToDelete.Add(FailingElementId);
-                            }
-
-                            hasFailure = true;
-
-                        }
-                    }
-                    else
-                    {
-                        if (failureMessageAccessor.HasResolutions())
-                        {
-                            failureAccessor.ResolveFailure(failureMessageAccessor);
-                            hasFailure = true;
-                        }
-                        else
-                        {
-                            failureAccessor.DeleteWarning(failureMessageAccessor);
-                        }
-
-                    }
-                }
-
-                if (failureSeverity == FailureSeverity.Warning)
-                {
-                    failureAccessor.DeleteWarning(failureMessageAccessor);
-                }
-            }
-
-            if (elementsToDelete.Count > 0)
-            {
-                failureAccessor.DeleteElements(elementsToDelete);
-            }
-
-            //use the following line to disable the message supressor after the external command ends
-            //CachedUiApp.Application.FailuresProcessing -= FaliureProcessor;
-            if (hasFailure)
-            {
-                e.SetProcessingResult(FailureProcessingResult.ProceedWithCommit);
-            }
-            e.SetProcessingResult(FailureProcessingResult.Continue);
-        }
 
         public ExternalDBApplicationResult OnShutdown(Autodesk.Revit.ApplicationServices.ControlledApplication app)
         {
@@ -109,20 +40,26 @@ namespace RevitToIFCBundle
             e.Succeeded = true;
         }
 
-        public static void ExportToIFC(DesignAutomationData data)
+        private void ExportToIFC(DesignAutomationData data)
         {
-
             if (data == null) throw new ArgumentNullException(nameof(data));
 
             Application rvtApp = data.RevitApp;
             if (rvtApp == null) throw new InvalidDataException(nameof(rvtApp));
 
-            string modelPath = data.FilePath;
-            if (String.IsNullOrWhiteSpace(modelPath)) throw new InvalidDataException(nameof(modelPath));
+            Document inputDoc = data.RevitDoc;
+            Document projectDocument = null;
 
-            Document doc = data.RevitDoc;
+            if (inputDoc.IsFamilyDocument)
+            {
+                projectDocument = LoadFamilyInDocument(rvtApp, inputDoc);
+            }
+            else
+            {
+                projectDocument = inputDoc;
+            }
 
-            if (doc == null) throw new InvalidOperationException("Could not open document.");
+            if (projectDocument == null) throw new InvalidOperationException("Could not open document.");
 
             // This part should be used to open a workshared model (maybe)
             //ModelPath path = ModelPathUtils.ConvertUserVisiblePathToModelPath(data.FilePath);
@@ -131,23 +68,106 @@ namespace RevitToIFCBundle
             //    DetachFromCentralOption = DetachFromCentralOption.DetachAndPreserveWorksets
             //};
 
-            //doc = rvtApp.OpenDocumentFile(path, opts);
+            // doc = rvtApp.OpenDocumentFile(path, opts);
 
-            if (doc == null) throw new InvalidOperationException("Could not open document.");
+            ExportDocumentToIFC(projectDocument);
 
-            using (Transaction transaction = new Transaction(doc))
+        }
+
+        private Document LoadFamilyInDocument(Application rvtApp, Document inputDoc)
+        {
+            // We must load the family in a new Revit doc
+            Document projectDocument = rvtApp.NewProjectDocument(UnitSystem.Metric);
+            Family family = inputDoc.LoadFamily(projectDocument);
+            FamilyPlacementType familyPlacementType = family.FamilyPlacementType;
+            ElementId familySymbolId = family.GetFamilySymbolIds().FirstOrDefault();
+
+            // Find the level whose Name is "Level 1"
+            FilteredElementCollector collector = new FilteredElementCollector(projectDocument);
+            List<Level> levels = collector.OfClass(typeof(Level)).ToElements().Cast<Level>().ToList();
+            Level firstLevel = levels.Where(l => l.Elevation == 0).FirstOrDefault();
+            ElementId levelId = firstLevel.Id;
+            Reference levelReference = new Reference(firstLevel);
+            Line line = Line.CreateBound(new XYZ(-5, 0, 0), new XYZ(5, 0, 0));
+
+            using (Transaction tx = new Transaction(projectDocument))
+            {
+                tx.Start("Create family instance");
+
+                if (familySymbolId != null)
+                {
+                    FamilySymbol familySymbol = projectDocument.GetElement(familySymbolId) as FamilySymbol;
+
+                    if (!familySymbol.IsActive) { familySymbol.Activate(); projectDocument.Regenerate(); }
+
+                    if (familySymbol != null)
+                    {
+                        switch (familyPlacementType)
+                        {
+                            case FamilyPlacementType.OneLevelBased:
+                                projectDocument.Create.NewFamilyInstance(new XYZ(), familySymbol, Autodesk.Revit.DB.Structure.StructuralType.NonStructural);
+                                break;
+                            case FamilyPlacementType.OneLevelBasedHosted:
+
+                                Wall wall = Wall.Create(projectDocument, line, levelId, false);
+
+                                tx.Commit();
+
+                                tx.Start("Keep going");
+                                projectDocument.Create.NewFamilyInstance(new XYZ(0, 0, 0), familySymbol, wall, Autodesk.Revit.DB.Structure.StructuralType.NonStructural);
+                                break;
+                            case FamilyPlacementType.TwoLevelsBased:
+                                projectDocument.Create.NewFamilyInstance(new XYZ(), familySymbol, firstLevel, Autodesk.Revit.DB.Structure.StructuralType.NonStructural);
+                                break;
+                            case FamilyPlacementType.ViewBased:
+                                throw new InvalidOperationException("This Revit family is an annotation, it cannot be exported to IFC.");
+                                break;
+                            case FamilyPlacementType.WorkPlaneBased:
+                                projectDocument.Create.NewFamilyInstance(levelReference, new XYZ(), new XYZ(), familySymbol);
+                                break;
+                            case FamilyPlacementType.CurveBased:
+                                projectDocument.Create.NewFamilyInstance(line, familySymbol, firstLevel, Autodesk.Revit.DB.Structure.StructuralType.NonStructural);
+                                break;
+                            case FamilyPlacementType.CurveBasedDetail:
+                                throw new InvalidOperationException("This Revit family is an annotation, it cannot be exported to IFC.");
+                                break;
+                            case FamilyPlacementType.CurveDrivenStructural:
+                                projectDocument.Create.NewFamilyInstance(new XYZ(), familySymbol, Autodesk.Revit.DB.Structure.StructuralType.NonStructural);
+                                break;
+                            case FamilyPlacementType.Adaptive:
+                                AdaptiveComponentInstanceUtils.CreateAdaptiveComponentInstance(projectDocument, familySymbol);
+                                break;
+                            case FamilyPlacementType.Invalid:
+                                throw new InvalidOperationException("This Revit family is invalid, it cannot be exported to IFC.");
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+
+                tx.Commit();
+            }
+
+            return projectDocument;
+        }
+
+
+
+        private void ExportDocumentToIFC(Document projectDocument)
+        {
+            using (Transaction transaction = new Transaction(projectDocument))
             {
                 transaction.Start("Export to IFC");
 
                 IFCExportOptions opt = new IFCExportOptions();
 
-                doc.Export(@".\", "output.ifc", opt);
+                projectDocument.Export(@".\", "output.ifc", opt);
                 transaction.Commit();
             }
 
             LogTrace("Saving IFC file...");
         }
-
         /// <summary>
         /// This will appear on the Design Automation output
         /// </summary>
--- RevitToIFCBundle/RevitToIFCFailuresProcessor.cs
+++ RevitToIFCBundle/RevitToIFCFailuresProcessor.cs
@@ -1,13 +1,12 @@
 ﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Events;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RevitToIFCBundle
 {
-    class RevitToIFCFailuresProcessor : IFailuresProcessor
+    class RevitToIFCFailuresProcessor
     {
         public void Dismiss(Document document)
         {
@@ -15,6 +14,100 @@ namespace RevitToIFCBundle
         }
 
         const int MAX_RESOLUTION_ATTEMPTS = 3;
+
+        public void FailureProcessor(object sender, FailuresProcessingEventArgs e)
+        {
+            RevitToIFCBundleApp.LogTrace("Enter Failures Processor event.");
+
+
+            FailuresAccessor failureAccessor = e.GetFailuresAccessor();
+
+            e.SetProcessingResult(ProcessFailure(failureAccessor));
+        }
+
+        private FailureProcessingResult ProcessFailure(FailuresAccessor failureAccessor)
+        {
+            bool hasFailure = false;
+            IList<FailureResolutionType> resolutionTypeList = new List<FailureResolutionType>();
+
+            List<FailureMessageAccessor> failureMessagesList = failureAccessor.GetFailureMessages().ToList();
+            List<ElementId> elementsToDelete = new List<ElementId>();
+            foreach (FailureMessageAccessor failureMessageAccessor in failureMessagesList)
+            {
+                // Log the failure encountered
+                RevitToIFCBundleApp.LogTrace("Default Resolution Caption: " + failureMessageAccessor.GetDefaultResolutionCaption());
+
+                // Check how many resolution types were attempted to try to prevent
+                // the application from entering an infinite loop.
+                resolutionTypeList = failureAccessor.GetAttemptedResolutionTypes(failureMessageAccessor);
+
+                if (resolutionTypeList.Count >= MAX_RESOLUTION_ATTEMPTS)
+                {
+                    RevitToIFCBundleApp.LogTrace("Failure: Attempted to resolve the failure "
+                       + failureMessageAccessor.GetDescriptionText() + " " + resolutionTypeList.Count
+                       + " times with resolution " + failureMessageAccessor.GetCurrentResolutionType()
+                       + ". Rolling back transaction.");
+                    return FailureProcessingResult.ProceedWithRollBack;
+                }
+                else
+                {
+                    FailureSeverity failureSeverity = failureAccessor.GetSeverity();
+                    string message = failureMessageAccessor.GetDefaultResolutionCaption();
+
+                    if (failureSeverity == FailureSeverity.Error)
+                    {
+                        // If the default resolution for the error results in deleting the elements delete them
+                        if (failureMessageAccessor.GetDefaultResolutionCaption().Equals("Delete Element(s)"))
+                        {
+                            //use the following lines to delete the warning elements
+                            List<ElementId> FailingElementIds = failureMessageAccessor.GetFailingElementIds().ToList();
+
+                            if (FailingElementIds.Count > 0)
+                            {
+                                ElementId FailingElementId = FailingElementIds[0];
+                                if (!elementsToDelete.Contains(FailingElementId))
+                                {
+                                    elementsToDelete.Add(FailingElementId);
+                                }
+
+                                hasFailure = true;
+
+                            }
+                        }
+                        else
+                        {
+                            if (failureMessageAccessor.HasResolutions())
+                            {
+                                failureAccessor.ResolveFailure(failureMessageAccessor);
+                                hasFailure = true;
+                            }
+                            else
+                            {
+                                failureAccessor.DeleteWarning(failureMessageAccessor);
+                            }
+                        }
+                    }
+
+                    if (failureSeverity == FailureSeverity.Warning)
+                    {
+                        failureAccessor.DeleteWarning(failureMessageAccessor);
+                    }
+                }
+            }
+
+            if (elementsToDelete.Count > 0)
+            {
+                failureAccessor.DeleteElements(elementsToDelete);
+            }
+
+            //use the following line to disable the message supressor after the external command ends
+            //CachedUiApp.Application.FailuresProcessing -= FaliureProcessor;
+            if (hasFailure)
+            {
+                return FailureProcessingResult.ProceedWithCommit;
+            }
+            return FailureProcessingResult.Continue;
+        }
 
         public FailureProcessingResult ProcessFailures(FailuresAccessor failuresAccessor)
         {
